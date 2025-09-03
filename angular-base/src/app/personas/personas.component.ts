ae19f81f7bf388fdf9dd150b2fe3f84cbfc128bf
Esteban M-meneses <tevan0304@gmail.com>
2025-09-03 16:03:31 -0500

    base angular

---
+++ angular-base/src/app/personas/personas.component.ts
@@ -0,0 +1,16 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "personas-app",
+    templateUrl: './personas.component.html',
+    styleUrl: './personas.personas.component.css'
+
+
+})
+
+export class MiComponentPersona{
+    public mensaje= 'SEGUNDO mensaje';
+    public otroMensaje= 'mi mensaje';
+
+    // LA LOGICA DE LOS COMPONENTES LISTAS ,  OBJETOS , ARREGLOS . ETC
+}
